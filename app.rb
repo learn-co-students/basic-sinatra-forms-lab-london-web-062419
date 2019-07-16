f852543ba98c2807a96c58e1e8e8f89cd1990fc3
github username <github email address>
2019-07-16 10:46:44 +0100

    Done.

--- app.rb
+++ app.rb
@@ -3,4 +3,23 @@ require 'sinatra/base'
 class App < Sinatra::Base
 
 
+    get '/newteam' do
+        erb :newteam
+    end
+
+    post '/team' do 
+    # this is how to display everything calling each param individually post each in the respective params
+    @name = params[:name]
+    @coach = params[:coach]
+    @pg = params[:pg]
+    @sg = params[:sg]
+    @pf = params[:pf]
+    @sf = params[:sf]
+    @c = params[:c]
+    erb :team
+    end
+
+
+
+
 end
